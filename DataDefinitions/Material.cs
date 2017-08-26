a1d33247ff344662e486bf16a17d223e472f22e1
Astilane <paul.capmarty@free.fr>
2017-08-26 14:14:05 +0200

    Add files via upload
    Correction de traduction

--- DataDefinitions/Material.cs
+++ DataDefinitions/Material.cs
@@ -78,47 +78,47 @@ namespace EddiDataDefinitions
         public static readonly Material Technetium = new Material("technetium", "Element", "Technétium", Rarity.VeryRare, "Tc", 1.3M, 1.5M);
         public static readonly Material Tellurium = new Material("tellurium", "Element", "Tellure", Rarity.VeryRare, "Te", 1.3M, 1.5M);
 
-        public static readonly Material AnomalousBulkScanData = new Material("bulkscandata", "Data", "Analyse de données de grandeur anormale", Rarity.VeryCommon);
-        public static readonly Material AtypicalDisruptedWakeEchoes = new Material("disruptedwakeechoes", "Data", "Échos de sillages atypiques perturbés", Rarity.VeryCommon);
+        public static readonly Material AnomalousBulkScanData = new Material("bulkscandata", "Data", "Fichier volumineux d'analyse anormal", Rarity.VeryCommon);
+        public static readonly Material AtypicalDisruptedWakeEchoes = new Material("disruptedwakeechoes", "Data", "Échos de sillages perturbés atypiques", Rarity.VeryCommon);
         public static readonly Material DistortedShieldCycleRecordings = new Material("shieldcyclerecordings", "Data", "Enregistrements de cycles de bouclier déformés", Rarity.VeryCommon);
-        public static readonly Material ExceptionalScrambledEmissionData = new Material("scrambledemissiondata", "Data", "Données d'émissions exceptionnellement brouillées", Rarity.VeryCommon);
+        public static readonly Material ExceptionalScrambledEmissionData = new Material("scrambledemissiondata", "Data", "Données d'émissions brouillées exceptionnelles", Rarity.VeryCommon);
         public static readonly Material SpecialisedLegacyFirmware = new Material("legacyfirmware", "Data", "Micrologiciel spécialisé périmé", Rarity.VeryCommon);
-        public static readonly Material UnusualEncryptedFiles = new Material("encryptedfiles", "Data", "Fichiers cryptés insolites", Rarity.VeryCommon);
+        public static readonly Material UnusualEncryptedFiles = new Material("encryptedfiles", "Data", "Fichiers cryptés inhabituels", Rarity.VeryCommon);
 
         public static readonly Material AnomalousFSDTelemetry = new Material("fsdtelemetry", "Data", "Télémétrie FSD anormale", Rarity.Common);
         public static readonly Material InconsistentShieldSoakAnalysis = new Material("shieldsoakanalysis", "Data", "Analyse d'absorption de bouclier incohérente", Rarity.Common);
-        public static readonly Material IrregularEmissionData = new Material("archivedemissiondata", "Data", "Données d'émissions irrégulières", Rarity.Common);
+        public static readonly Material IrregularEmissionData = new Material("archivedemissiondata", "Data", "Données d'émissions aberrantes", Rarity.Common);
         public static readonly Material ModifiedConsumerFirmware = new Material("consumerfirmware", "Data", "Micrologiciel consommateur modifié", Rarity.Common);
-        public static readonly Material TaggedEncryptionCodes = new Material("encryptioncodes", "Data", "Codes de cryptage marqué", Rarity.Common);
+        public static readonly Material TaggedEncryptionCodes = new Material("encryptioncodes", "Data", "Clés de cryptage balisées", Rarity.Common);
         public static readonly Material UnidentifiedScanArchives = new Material("scanarchives", "Data", "Données d'analyse archivées non identifiées", Rarity.Common);
 
-        public static readonly Material ClassifiedScanDatabanks = new Material("scandatabanks", "Data", "Analyse de banques de données classifiées", Rarity.Standard);
+        public static readonly Material ClassifiedScanDatabanks = new Material("scandatabanks", "Data", "Banques de données d'analyse classifiées", Rarity.Standard);
         public static readonly Material CrackedIndustrialFirmware = new Material("industrialfirmware", "Data", "Micrologiciel industriel piraté", Rarity.Standard);
-        public static readonly Material OpenSymmetricKeys = new Material("symmetrickeys", "Data", "Clefs à ouverture symétriques", Rarity.Standard);
-        public static readonly Material StrangeWakeSolutions = new Material("wakesolutions", "Data", "Solutions de sillage anormales", Rarity.Standard);
+        public static readonly Material OpenSymmetricKeys = new Material("symmetrickeys", "Data", "clés symétriques ouvertes", Rarity.Standard);
+        public static readonly Material StrangeWakeSolutions = new Material("wakesolutions", "Data", "Solutions de sillages anormales", Rarity.Standard);
         public static readonly Material UnexpectedEmissionData = new Material("emissiondata", "Data", "Données d'émissions inattendues", Rarity.Standard);
         public static readonly Material UntypicalShieldScans = new Material("shielddensityreports", "Data", "Analyses de bouclier atypique", Rarity.Standard);
         public static readonly Material UnknownShipSignature = new Material("unknownshipsignature", "Data", "Signature de vaisseau inconu", Rarity.Standard);
         public static readonly Material UnknownWakeScan = new Material("unknownwakedata", "Data", "Données de sillage inconu", Rarity.Standard);
 
         public static readonly Material AberrantShieldPatternAnalysis = new Material("shieldpatternanalysis", "Data", "Analyse de modèle de bouclier aberrante", Rarity.Rare);
-        public static readonly Material AtypicalEncryptionArchives = new Material("encryptionarchives", "Data", "Capture d'encrypteurs adaptatifs", Rarity.Rare);
+        public static readonly Material AtypicalEncryptionArchives = new Material("encryptionarchives", "Data", "Archives cryptées atypiques", Rarity.Rare);
         public static readonly Material DecodedEmissionData = new Material("decodedemissiondata", "Data", "Données d'émissions décodées", Rarity.Rare);
         public static readonly Material DivergentScanData = new Material("encodedscandata", "Data", "Données d'analyse divergentes", Rarity.Rare);
         public static readonly Material EccentricHyperspaceTrajectories = new Material("hyperspacetrajectories", "Data", "Trajectoires hyperespace exentriques", Rarity.Rare);
         public static readonly Material SecurityFirmwarePatch = new Material("securityfirmware", "Data", "Mise à jour de micrologiciel de sécurité", Rarity.Rare);
 
-        public static readonly Material AbnormalCompactEmissionData = new Material("compactemissionsdata", "Data", "Données d'émissions anormales compactes", Rarity.VeryRare);
-        public static readonly Material AdaptiveEncryptorsCapture = new Material("adaptiveencryptors", "Data", "Capture d'encrypteurs adaptatifs", Rarity.VeryRare);
-        public static readonly Material ClassifiedScanFragment = new Material("classifiedscandata", "Data", "Portion de scan classifiées", Rarity.VeryRare);
+        public static readonly Material AbnormalCompactEmissionData = new Material("compactemissionsdata", "Data", "Données d'émissions compactes anormales", Rarity.VeryRare);
+        public static readonly Material AdaptiveEncryptorsCapture = new Material("adaptiveencryptors", "Data", "Capture de cryptage évolutif", Rarity.VeryRare);
+        public static readonly Material ClassifiedScanFragment = new Material("classifiedscandata", "Data", "Données d'analyse classifiées parcellaires", Rarity.VeryRare);
         public static readonly Material DataminedWakeExceptions = new Material("dataminedwake", "Data", "Exploration de données de sillage anormales", Rarity.VeryRare);
         public static readonly Material ModifiedEmbeddedFirmware = new Material("embeddedfirmware", "Data", "logiciel intégré modifié", Rarity.VeryRare);
-        public static readonly Material PeculiarShieldFrequencyData = new Material("shieldfrequencydata", "Data", "Fréquence de donnée de bouclier particulière", Rarity.VeryRare);
+        public static readonly Material PeculiarShieldFrequencyData = new Material("shieldfrequencydata", "Data", "Données de Fréquences de bouclier Singulières", Rarity.VeryRare);
 
-        public static readonly Material BasicConductors = new Material("basicconductors", "Manufactured", "Conducteurs basiques", Rarity.VeryCommon);
-        public static readonly Material ChemicalStorageUnits = new Material("", "Manufactured", "Unité de stockage chimique", Rarity.VeryCommon);
+        public static readonly Material BasicConductors = new Material("basicconductors", "Manufacturé", "Conducteurs simples", Rarity.VeryCommon);
+        public static readonly Material ChemicalStorageUnits = new Material("", "Manufacturé", "Unité de stockage chimique", Rarity.VeryCommon);
         public static readonly Material CompactComposites = new Material("", "Manufactured", "Composites compacts", Rarity.VeryCommon);
-        public static readonly Material CrystalShards = new Material("crystalshards", "Manufactured", "Fragments de Cristal", Rarity.VeryCommon);
+        public static readonly Material CrystalShards = new Material("crystalshards", "Manufactured", "Eclat de cristal", Rarity.VeryCommon);
         public static readonly Material GridResistors = new Material("gridresistors", "Manufactured", "Résistances à grille", Rarity.VeryCommon);
         public static readonly Material HeatConductionWiring = new Material("heatconductionwiring", "Manufactured", "Cablage de conduction thermique", Rarity.VeryCommon);
         public static readonly Material MechanicalScrap = new Material("mechanicalscrap", "Manufactured", "Ferraille mécanique", Rarity.VeryCommon);
@@ -150,8 +150,8 @@ namespace EddiDataDefinitions
 
         public static readonly Material ChemicalManipulators = new Material("chemicalmanipulators", "Manufactured", "Manipulateur chimique", Rarity.Rare);
         public static readonly Material CompoundShielding = new Material("compoundshielding", "Manufactured", "Protection composite", Rarity.Rare);
-        public static readonly Material ConductivePolymers = new Material("conductivepolymers", "Manufactured", "Polymères conductifs", Rarity.Rare);
-        public static readonly Material ConfigurableComponents = new Material("configurablecomponents", "Manufactured", "Composants configurables", Rarity.Rare);
+        public static readonly Material ConductivePolymers = new Material("conductivepolymers", "Manufactured", "Conducteurs en polymères", Rarity.Rare);
+        public static readonly Material ConfigurableComponents = new Material("configurablecomponents", "Manufactured", "Composant paramétrables", Rarity.Rare);
         public static readonly Material HeatVanes = new Material("heatvanes", "Manufactured", "Vannes thermiques", Rarity.Rare);
         public static readonly Material PolymerCapacitors = new Material("polymercapacitors", "Manufactured", "Condensateurs en polymères", Rarity.Rare);
         public static readonly Material ProprietaryComposites = new Material("fedproprietarycomposites", "Manufactured", "Composites brevetés", Rarity.Rare);
@@ -159,12 +159,12 @@ namespace EddiDataDefinitions
         public static readonly Material RefinedFocusCrystals = new Material("refinedfocuscrystals", "Manufactured", "Cristaux de focalisation raffinés", Rarity.Rare);
         public static readonly Material ThermicAlloys = new Material("thermicalloys", "Manufactured", "Alliages thermiques", Rarity.Rare);
 
-        public static readonly Material BiotechConductors = new Material("biotechconductors", "Manufactured", "Conducteurs biotechnologiques", Rarity.VeryRare);
+        public static readonly Material BiotechConductors = new Material("biotechconductors", "Manufactured", "Conducteurs biotechniques", Rarity.VeryRare);
         public static readonly Material CoreDynamicsComposites = new Material("fedcorecomposites", "Manufactured", "Composites Core Dynamics", Rarity.VeryRare);
-        public static readonly Material ExquisiteFocusCrystals = new Material("exquisitefocuscrystals", "Manufactured", "Cristaux de focalisation exquis", Rarity.VeryRare);
-        public static readonly Material ImperialShielding = new Material("imperialshielding", "Manufactured", "Blindage impérial", Rarity.VeryRare);
+        public static readonly Material ExquisiteFocusCrystals = new Material("exquisitefocuscrystals", "Manufactured", "Cristaux de focalisation sans défaut", Rarity.VeryRare);
+        public static readonly Material ImperialShielding = new Material("imperialshielding", "Manufactured", "Protection impériale", Rarity.VeryRare);
         public static readonly Material ImprovisedComponents = new Material("improvisedcomponents", "Manufactured", "Composants improvisés", Rarity.VeryRare);
-        public static readonly Material MilitaryGradeAlloys = new Material("militarygradealloys", "Manufactured", "Alliage de qualité militaire", Rarity.VeryRare);
+        public static readonly Material MilitaryGradeAlloys = new Material("militarygradealloys", "Manufactured", "Alliages militaires", Rarity.VeryRare);
         public static readonly Material MilitarySupercapacitors = new Material("militarysupercapacitors", "Manufactured", "Supercondensateurs militaires", Rarity.VeryRare);
         public static readonly Material PharmaceuticalIsolators = new Material("pharmaceuticalisolators", "Manufactured", "Isolants pharmaceutiques", Rarity.VeryRare);
         public static readonly Material ProtoHeatRadiators = new Material("protoheatradiators", "Manufactured", "Proto-radiateurs", Rarity.VeryRare);
